235ef7f473a854d975c8a286cb5b496d0c78794d
Roma <forwm.ru@gmail.com>
2019-07-23 02:15:38 +0300

    fixed localhost

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('dist', path.join(__dirname, 'dist'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/dist'));
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}!`);
+});
+
+  // .use(express.static(path.join(__dirname, 'dist')))
+  // .set('dist', path.join(__dirname, 'dist'))
+  // .set('view engine', 'ejs')
+  // .get('/', (req, res) => res.render('pages/index'))
+  // .listen(PORT, () => console.log(`Listening on ${ PORT }`))
