85cc412c829b84137f3be83fde7b44417672984e
Roma <forwm.ru@gmail.com>
2019-07-26 23:47:59 +0300

    Try to get hosting_revert_1

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const mongoose = require('mongoose');
 const PORT = process.env.PORT || 5000;
-
 const app = express();
 
 app.use(bodyParser.json());
@@ -12,8 +10,8 @@ app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
 });
 
-  // .use(express.static(path.join(__dirname, 'dist')))
-  // .set('dist', path.join(__dirname, 'dist'))
-  // .set('view engine', 'ejs')
-  // .get('/', (req, res) => res.render('pages/index'))
-  // .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// .use(express.static(path.join(__dirname, 'dist')))
+// .set('dist', path.join(__dirname, 'dist'))
+// .set('view engine', 'ejs')
+// .get('/', (req, res) => res.render('pages/index'))
+// .listen(PORT, () => console.log(`Listening on ${ PORT }`))
