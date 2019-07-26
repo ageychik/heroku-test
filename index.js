e5dbdf28d6987eb83d5828388d7aa7ac2e25ff41
Roma <forwm.ru@gmail.com>
2019-07-26 22:20:43 +0300

    added db connect

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || 5000;
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/dist'));
+
+require('dotenv').config();
+
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
 });
+
+mongoose.connect(process.env.DB_NAME)
+    .then(() => {
+        console.log('database connected');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
   // .use(express.static(path.join(__dirname, 'dist')))
   // .set('dist', path.join(__dirname, 'dist'))
