4a7e4f7f6d46409ab0238dee8c57189e2e55526a
Roma <forwm.ru@gmail.com>
2019-07-26 23:54:25 +0300

    Try to get hosting_2

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(bodyParser.json());
